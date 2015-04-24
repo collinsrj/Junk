eb1b292b2241e32d7bff4a0b4dba063131d57e13
Robert Collins <collinsrj@gmail.com>
2015-04-24 18:28:25 +0100

    Initial Commit

---
+++ Junk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Junk
+//
+//  Created by Robert Collins on 24/04/2015.
+//  Copyright (c) 2015 Robert Collins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
